5a2653bbd900d6d423b69090f6895149092f1865
Farhan Sajid <fsreg11194@gmail.com>
2023-05-22 23:59:42 +0600

    fixed admin panel css

--- admin/header.php
+++ admin/header.php
@@ -17,8 +17,8 @@ if(isset($_SESSION["role"])){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>RaimentsGlobal</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link href="../css/style.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet">
@@ -30,7 +30,7 @@ if(isset($_SESSION["role"])){
 <body>
 <nav class="nav">
     <div class="container nav__body">
-        <a href="/"><img src="img/logo.png"/></a>
+        <a href="/"><img src="../img/logo.png"/></a>
         <form class="nav__form" action="products.php" method="get">
             <input type="text" class="nav__form__input" id="nav__form__input" name="search" placeholder="Name">
             <button type="submit" class="btn"><i class="lni lni-search-alt"></i></button>
